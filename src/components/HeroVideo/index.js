d9a2e7f4596ef146820cefccbdcd23032374ddb6
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-10-08 13:16:54 -0400

    Updated Footer

--- src/components/HeroVideo/index.js
+++ src/components/HeroVideo/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./index.css";
+import Video from "../../components/Video/index";
 
 function HeroVideo() {
   return (
-    <section id="hero">
-      <h1 className="title hero-title">Designer. Engineer.</h1>
-      <h2 className="title hero-subtitle">
-        I am a creative web developer in Raleigh.
-      </h2>
-    </section>
+    <div>
+      <Video />
+      <section id="hero">
+        <h1 className="title hero-title">Designer. Engineer.</h1>
+        <h2 className="title hero-subtitle">
+          I am a creative web developer in Raleigh.
+        </h2>
+      </section>
+    </div>
   );
 }
 
--- src/pages/Work/index.js
+++ src/pages/Work/index.js
@@ -1,31 +1,11 @@
 import React from "react";
-import Video from "../../components/Video/index";
 import HeroVideo from "../../components/HeroVideo/index";
 import "./index.css";
 
 function Work() {
   return (
     <div>
-      <Video />
       <HeroVideo />
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
-      <h1>Blank Line</h1>
     </div>
   );
 }
