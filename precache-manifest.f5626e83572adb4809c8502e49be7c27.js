603fd569c9720211cf82a15cc0fbdfa671e14d87
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-10-28 12:56:24 -0400

    Updates

---
+++ precache-manifest.f5626e83572adb4809c8502e49be7c27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9938d2b2dc76fe7ab7345637d6becd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c36650db42240b409715",
+    "url": "/static/css/main.3728ab4e.chunk.css"
+  },
+  {
+    "revision": "29efe12a7c1de1212ba3",
+    "url": "/static/js/2.311b4923.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.311b4923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c36650db42240b409715",
+    "url": "/static/js/main.bb813cfe.chunk.js"
+  },
+  {
+    "revision": "644216488884bb34dc99",
+    "url": "/static/js/runtime-main.48de65ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
